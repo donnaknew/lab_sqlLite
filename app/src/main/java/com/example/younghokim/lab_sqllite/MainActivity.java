cbb1f9bef2f0766b1828c930afc53d637e8d0c66
younghoKim <donnaknew@naver.com>
2015-11-26 13:07:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/younghokim/lab_sqllite/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.younghokim.lab_sqllite;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
+    String dbName = "idList.db"; // name of Database;
+    String tableName = "idListTable"; // name of Table;
+    int dbMode = Context.MODE_PRIVATE;
+
+
+    // layout object
+    EditText mEtName;
+    EditText mEtNumberName;
+    EditText mEtUpdateName;
+    Button mBtInsert;
+    Button mBtRead;
+    Button mBtDelete;
+    Button mBtUpdate;
+
+
+    ListView mList;
+    ArrayAdapter<String> baseAdapter;
+    ArrayList<String> nameList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // // Database 생성 및 열기
+        db = openOrCreateDatabase(dbName,dbMode,null);
+        // 테이블 생성
+        createTable();
+
+        mEtName = (EditText) findViewById(R.id.et_text);
+        mEtNumberName = (EditText)findViewById(R.id.et_num);
+        mEtUpdateName = (EditText)findViewById(R.id.et_update);
+        mBtInsert = (Button) findViewById(R.id.bt_insert);
+        mBtRead = (Button) findViewById(R.id.bt_read);
+        mBtDelete = (Button)findViewById(R.id.bt_delete);
+        mBtUpdate = (Button)findViewById(R.id.bt_update);
+        ListView mList = (ListView) findViewById(R.id.list_view);
+
+        mBtInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mEtName.getText().toString();
+                insertData(name);
+            }
+        });
+
+        mBtRead = (Button) findViewById(R.id.bt_read);
+        mBtRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nameList.clear();
+                selectAll();
+                baseAdapter.notifyDataSetChanged(); // this code will change listView when list changed
+            }
+        });
+
+        mBtDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int delName = Integer.parseInt(mEtNumberName.getText().toString());
+                removeData(delName);
+            }
+        });
+
+        mBtUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int updateNum = Integer.parseInt(mEtNumberName.getText().toString());
+                String updateName = mEtUpdateName.getText().toString();
+                updateData(updateNum, updateName);
+            }
+        });
+
+        // Create listview
+        nameList = new ArrayList<String>();
+        baseAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, nameList);
+        mList.setAdapter(baseAdapter);
+
+    }
+
+    // Table 생성
+    public void createTable() {
+        try {
+            String sql = "create table " + tableName + "(id integer primary key autoincrement, " + "name text not null)";
+            db.execSQL(sql);
+        } catch (android.database.sqlite.SQLiteException e) {
+            Log.d("Lab sqlite", "error: " + e);
+        }
+    }
+
+    // Table 삭제
+    public void removeTable() {
+        String sql = "drop table " + tableName;
+        db.execSQL(sql);
+    }
+
+    // Data 추가
+    public void insertData(String name) {
+        String sql = "insert into " + tableName + " values(NULL, '" + name + "');";
+        db.execSQL(sql);
+    }
+
+    // Data 업데이트
+    public void updateData(int index, String name) {
+        String sql = "update " + tableName + " set name = '" + name + "' where id = " + index + ";";
+        db.execSQL(sql);
+    }
+
+    // Data 삭제
+    public void removeData(int index) {
+        String sql = "delete from " + tableName + " where id = " + index + ";";
+        db.execSQL(sql);
+    }
+
+    // Data 읽기(꺼내오기)
+    public void selectData(int index) {
+        String sql = "select * from " + tableName + " where id = " + index + ";";
+        Cursor result = db.rawQuery(sql, null);
+
+        // result(Cursor 객체)가 비어 있으면 false 리턴
+        if (result.moveToFirst()) {
+            int id = result.getInt(0);
+            String name = result.getString(1);
+//            Toast.makeText(this, "index= " + id + " name=" + name, Toast.LENGTH_LONG).show();
+            Log.d("lab_sqlite", "\"index= \" + id + \" name=\" + name ");
+        }
+        result.close();
+    }
+
+    // 모든 Data 읽기
+    public void selectAll() {
+        String sql = "select * from " + tableName + ";";
+        Cursor results = db.rawQuery(sql, null);
+        results.moveToFirst();
+
+        while (!results.isAfterLast()) {
+            int id = results.getInt(0);
+            String name = results.getString(1);
+//            Toast.makeText(this, "index= " + id + " name=" + name, Toast.LENGTH_LONG).show();
+            Log.d("lab_sqlite", "index= " + id + " name=" + name);
+
+            nameList.add(name);
+            results.moveToNext();
+        }
+        results.close();
+    }
+
+}
